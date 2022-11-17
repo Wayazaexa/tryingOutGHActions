f71ef8a37f8cebe071765b33a8e7654f4de3468b
Joakim Englund <joakimenglund@protonmail.com>
2022-11-17 10:20:12 +0100

    Update sample.c
    Changed the use of gets() to fgets().

--- sample.c
+++ sample.c
@@ -3,27 +3,28 @@
 
 int main(int argc, char **argv)
 {
-    char buff[15];
+	int bufferSize = 15;
+	char buff[bufferSize];
 	int pass = 0;
 
-    printf("\n Enter the password : \n");
-    gets(buff);
+	printf("\n Enter the password : \n");
+	fgets(buff, bufferSize, stdin);
 
-    if(strcmp(buff, "thegeekstuff"))
-    {
-        printf ("\n Wrong Password \n");
-    }
-    else
-    {
-        printf ("\n Correct Password \n");
-        pass = 1;
-    }
+	if(strcmp(buff, "thegeekstuff"))
+	{
+		printf ("\n Wrong Password \n");
+	}
+	else
+	{
+		printf ("\n Correct Password \n");
+		pass = 1;
+	}
 
-    if(pass)
-    {
-       /* Now Give root or admin rights to user*/
-        printf ("\n Root privileges given to the user \n");
-    }
+	if(pass)
+	{
+		/* Now Give root or admin rights to user*/
+		printf ("\n Root privileges given to the user \n");
+	}
 
-    return 0;
+	return 0;
 }
